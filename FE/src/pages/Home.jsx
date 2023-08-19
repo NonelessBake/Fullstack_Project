e5469682d1aeb97edf245efac8e8296c852d7f79
NonelessBake <bakeferocious@gmail.com>
2023-08-19 22:06:42 +0700

    FE

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -1,21 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "../assets/style/home.css";
 import Category from "../components/Category";
 const Home = () => {
-  return (
-    <section className="home">
-      <div className="slide">
-        <button className="change-slide slide-left"> &lt; </button>
-        <img
-          className="img-slide"
-          src="https://wpbingosite.com/wordpress/funio/wp-content/webp-express/webp-images/uploads/2020/12/img5-6.jpg.webp"
-          alt=""
-        />
-        <button className="change-slide slide-right"> &gt;</button>
-      </div>
-      <div>asdasd</div>
-    </section>
-  );
+  const { params } = useParams();
+  console.log(params);
+  return <section className="home"></section>;
 };
 
 export default Home;
