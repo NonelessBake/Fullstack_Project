c80089c91613d3b445ce22ace2498a039f26b1ba
NonelessBake <bakeferocious@gmail.com>
2023-08-20 22:14:23 +0700

    Update Home

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -14,39 +14,68 @@ const Home = () => {
     fetchBanner();
   }, []);
   const imagesBanner = banner.map((item) => item.img);
-  console.log(imagesBanner);
+
+  const shopStyleImgs = [
+    {
+      content: "Classic Traditional",
+      src: "https://wpbingosite.com/wordpress/funio/wp-content/webp-express/webp-images/uploads/2020/12/img5-6.jpg.webp",
+    },
+    {
+      content: "Modern Boho",
+      src: "https://wpbingosite.com/wordpress/funio/wp-content/webp-express/webp-images/uploads/2020/12/img5-7.jpg.webp",
+    },
+    {
+      content: "Retro Mid Century",
+      src: "https://wpbingosite.com/wordpress/funio/wp-content/webp-express/webp-images/uploads/2020/12/img5-8.jpg.webp",
+    },
+  ];
+
   return (
     <section className="home">
       <h1>Top Categories</h1>
       <Category />
       <section className="banner-direct">
-        <div className="banner-container-home">
-          <div className="content-banner">
-            <p>FROM LOVESEATS TO SECTIONALS.</p>
-            <h1>Comfy Lounging</h1>
-            <p className="banner-link-to">
-              <NavLink>Explore Collection</NavLink>
-            </p>
+        {imagesBanner.map((item, index) => (
+          <div key={index} className="banner-container-home">
+            <div className="content-banner">
+              <p>
+                {index === 0
+                  ? "FROM LOVESEATS TO SECTIONALS."
+                  : "SALE UP TO 20% OFF ALL ITEMS"}
+              </p>
+              <h1>Comfy Lounging</h1>
+              <p className="banner-link-to">
+                <NavLink>Explore Collection</NavLink>
+              </p>
+            </div>
+            <div className="banner-img">
+              <Link>
+                <img src={item} alt={item} />
+              </Link>
+            </div>
           </div>
-          <img
-            src={imagesBanner[0]}
-            alt={imagesBanner[0]}
-            className="banner-img"
-          />
-        </div>
-        <div className="banner-container-home">
-          <div className="content-banner">
-            <p>SALE UP TO 20% OFF ALL ITEMS</p>
-            <h1>Scandinavian Style</h1>
-            <p className="banner-link-to">
-              <NavLink>Explore Collection</NavLink>
-            </p>
-          </div>
-          <img
-            src={imagesBanner[1]}
-            alt={imagesBanner[1]}
-            className="banner-img"
-          />
+        ))}
+      </section>
+      <section className="shop-style">
+        <h1>Shop by style</h1>
+        <div className="shop-by-style ">
+          {shopStyleImgs.map((item, index) => (
+            <div key={index} className="shop-by-style-cotainer">
+              <div className="bg-highlight bg-highlight-img ">
+                <div className="shop-style-content-container">
+                  <h2 className="style-content">{item.content}</h2>
+                  <p className="hidden-link-to">
+                    <Link>Explore now</Link>
+                  </p>
+                </div>
+                <div className="background-shop-style-bottom ">
+                  <Link>
+                    <img src={item.src} alt={item.content} />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </section>
