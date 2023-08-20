d2ad851f1e59af2d9b39485284877b247257de01
NonelessBake <bakeferocious@gmail.com>
2023-08-20 22:25:49 +0700

    Fix

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -35,26 +35,30 @@ const Home = () => {
       <h1>Top Categories</h1>
       <Category />
       <section className="banner-direct">
-        {imagesBanner.map((item, index) => (
-          <div key={index} className="banner-container-home">
-            <div className="content-banner">
-              <p>
-                {index === 0
-                  ? "FROM LOVESEATS TO SECTIONALS."
-                  : "SALE UP TO 20% OFF ALL ITEMS"}
-              </p>
-              <h1>Comfy Lounging</h1>
-              <p className="banner-link-to">
-                <NavLink>Explore Collection</NavLink>
-              </p>
+        <div className="banner-direct-container">
+          {imagesBanner.map((item, index) => (
+            <div key={index} className="banner-container-home">
+              <div className="banner-highlight">
+                <div className="content-banner ">
+                  <p>
+                    {index === 0
+                      ? "FROM LOVESEATS TO SECTIONALS."
+                      : "SALE UP TO 20% OFF ALL ITEMS"}
+                  </p>
+                  <h1>Comfy Lounging</h1>
+                  <p className="banner-link-to">
+                    <NavLink>Explore Collection</NavLink>
+                  </p>
+                </div>
+                <div className="banner-img">
+                  <Link>
+                    <img src={item} alt={item} />
+                  </Link>
+                </div>
+              </div>
             </div>
-            <div className="banner-img">
-              <Link>
-                <img src={item} alt={item} />
-              </Link>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </section>
       <section className="shop-style">
         <h1>Shop by style</h1>
