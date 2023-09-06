783193910b9a9ede8c568db2baf65e827590d112
NonelessBake <bakeferocious@gmail.com>
2023-09-06 20:08:34 +0700

    1

--- FE/src/components/Checkout/CheckoutCartItem.jsx
+++ FE/src/components/Checkout/CheckoutCartItem.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-const CheckoutCartItem = () => {
+const CheckoutCartItem = (newProps) => {
+  const { cartItem } = newProps;
+  const { price, discount, img, quantity, name } = cartItem;
+  const newPrice = price * (1 - discount / 100);
   return <div></div>;
 };
 
