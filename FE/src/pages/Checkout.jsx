2a77ce6fc76d855c3d6c1287e19300e28c26747d
NonelessBake <bakeferocious@gmail.com>
2023-09-11 23:02:20 +0700

    1

--- FE/src/pages/Checkout.jsx
+++ FE/src/pages/Checkout.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import CheckoutForm from "../components/Checkout/CheckoutForm";
 import { ContextValue } from "../contexts/ContextProvider";
 import { Link } from "react-router-dom";
+import { BsArrowLeft } from "react-icons/bs";
+
 import "../assets/style/checkout.css";
 const Checkout = () => {
   const { cart } = useContext(ContextValue);
@@ -11,7 +13,10 @@ const Checkout = () => {
         <div className="checkout-empty">
           <div>
             <div className="empty-content">Your cart is currently empty</div>
-            <Link to="/shop">Return to shop</Link>
+            <Link to="/shop">
+              <BsArrowLeft />
+              <span style={{ padding: "0 10px" }}>Return to shop</span>
+            </Link>
           </div>
         </div>
       ) : (
