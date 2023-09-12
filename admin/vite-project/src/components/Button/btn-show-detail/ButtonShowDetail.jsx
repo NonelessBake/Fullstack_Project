615acc514169232276d233c87a0ba3106ea0d119
NonelessBake <bakeferocious@gmail.com>
2023-09-12 13:11:47 +0700

    up

--- admin/vite-project/src/components/Button/btn-show-detail/ButtonShowDetail.jsx
+++ admin/vite-project/src/components/Button/btn-show-detail/ButtonShowDetail.jsx
@@ -1,31 +1,15 @@
 import React from "react";
 
 export default function ButtonShowDetail(props) {
-<<<<<<< HEAD
-    let ShowBoxModalDetail = (e) => {
-       
-    }
-    return (
-        <button type="button"
-            onClick={ShowBoxModalDetail}
-            data-bs-toggle={"modal"} 
-            data-bs-target={"#staticBackdrop" + props.id}>
-            Detail
-        </button>
-    )
-=======
-  let ShowBoxModalDetail = (e) => {
-    console.log(props.name);
-  };
+  let ShowBoxModalDetail = (e) => {};
   return (
     <button
       type="button"
       onClick={ShowBoxModalDetail}
       data-bs-toggle={"modal"}
-      data-bs-target={"#staticBackdrop" + props.name}
+      data-bs-target={"#staticBackdrop" + props.id}
     >
       Detail
     </button>
   );
->>>>>>> FE
 }
